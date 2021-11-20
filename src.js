70c24d69b4489525d868bb5088a7225e4a73deca
DavidCharif <53876827+DavidCharif@users.noreply.github.com>
2021-11-20 14:39:24 -0500

    Añado "calcularPocentajes" y corrijo registrarHC

--- src.js
+++ src.js
@@ -1,23 +1,37 @@
-/*
-Boceto Historia de Usuario 3
-
- const calcularPorcentajes = (limiteCO, limiteCO2, limiteHC, limiteO2, valorCO, valorCO2, valorHC, valorO2) => {
-     let porcentajeCO = 0
-     let porcentajeCO2 = 0
-     let porcentajeHC = 0
-     let porcentajeO2 = 0
+/* Historia de usuario 3 */ 
+const calcularPocentajes=  (limiteCO, limiteCO2, limiteHC, limiteO2, valorCO, valorCO2, valorHC, valorO2) => {
+     
+     let porcentajeCO;
+     let porcentajeCO2;
+     let porcentajeHC;
+     let porcentajeO2;
     
     if (limiteCO !== 0) {
-
+        porcentajeCO = (valorCO * 100) / limiteCO;
      } else {
-
+         porcentajeCO = 0
     }
-
-    return {}
+    if (limiteCO2 !== 0) {
+        porcentajeCO2 = (valorCO2 * 100) / limiteCO2;
+     } else {
+         porcentajeCO2 = 0
+    }
+    if (limiteHC !== 0) {
+        porcentajeHC = (valorHC * 100) / limiteHC;
+     } else {
+         porcentajeHC = 0
+    }
+    if (limiteO2 !== 0) {
+        porcentajeO2 = (valorO2 * 100) / limiteO2;
+     } else {
+         porcentajeO2 = 0
+    }
+    let objetoReturn = {"porcentajeCO":porcentajeCO,"porcentajeCO2":porcentajeCO2,"porcentajeHC":porcentajeHC,"porcentajeO2" : porcentajeO2};
+    return objetoReturn;
 }
-*/
 
 
+/* Historia de usuario 4 */ 
 /*
 Jennifer
 */
@@ -59,25 +73,25 @@ global.rangosHC = [
     {etiqueta: 'Parametro HC fuera de rango', de: 10001, hasta: 11000}
 ]
 
+const registrarHC = value => {
 let estandarDesde = global.rangosHC[0].de;
 let estandarHasta = global.rangosHC[0].hasta;
 let etiquetaEstandar = global.rangosHC[0].etiqueta;
-// Aparentemente no es necesario incluir los limites
-/*
+let fueraRangoEtiqueta = global.rangosHC[1].etiqueta;
 let fueraDeRangoDesde = global.rangosHC[1].de;
 let fueraDeRangoHasta = global.rangosHC[1].hasta;
-*/
-const registrarHC = value => {
     if(value >= estandarDesde && value <= estandarHasta){
         console.log(etiquetaEstandar);
         return etiquetaEstandar;
     }
-    else if (value < estandarDesde || value > estandarHasta){
-    console.log("fuera_de_rango")
+    else if (value >= fueraDeRangoDesde && value <= fueraDeRangoHasta){
+    
+        return fueraRangoEtiqueta
+        
+    } else {
         return "fuera_de_rango"
     }
 }
-
 
 /*
 Germán
