a677d99c010ffde3bc935ded19b788a7288e3549
Jennifer Alarcon Serna <jenniferalarconserna@gmail.com>
2021-12-04 00:48:01 -0500

    Metodo GET ranges_parameters y pruebas con el bot del metodo GET limits 10/10

--- src.js
+++ src.js
@@ -5,13 +5,20 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
+let ranges_parameters = [{"etiqueta":"Monoxido de carbono","de":0,"hasta":10},{"etiqueta":"Dioxido de carbono","de":0,"hasta":20},{"etiqueta":"Hidrocarburos","de":0,"hasta":10000},{"etiqueta":"Oxigeno","de":0,"hasta":22}]
+
+// let limits = {"Parametro CO en rango estandar":10,"Parametro CO2 en rango estandar":20,"Parametro HC en rango estandar":10000,"Parametro O2 en rango estandar":22}
+
 // Esqueleto del controlador
 app.get("/api/emission-measurement/ranges_parameters", (req, res) => {
-
+    res.json(ranges_parameters);
 });
 
 app.get("/api/emission-measurement/limits", (req, res) => {
-
+  // Pruebas con el bot 
+  // res.send({
+  //   "limits": limits
+  // });
 });
 
 app.post("/api/emission-measurement/ranges_parameters", (req, res) => {
@@ -20,6 +27,8 @@ app.post("/api/emission-measurement/ranges_parameters", (req, res) => {
 
 });
 
+// const port = 3000;
+// app.listen(port, () => { console.log(`Example app listening at http://localhost:${port}`) })
 
 
 module.exports = app;
