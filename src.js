1d64f0b6ce8f978ecc96bcf4e96ab2e22fe0869b
Jennifer Alarcon Serna <jenniferalarconserna@gmail.com>
2021-12-07 11:31:45 -0500

    Archivo actualizado y probado con el bot 10/10 faltaba resolver la promesa

--- src.js
+++ src.js
@@ -1,34 +1,20 @@
-// Configuración inicial
-const express = require('express');
-const app = express();
-
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
-
-let ranges_parameters = [{"etiqueta":"Monoxido de carbono","de":0,"hasta":10},{"etiqueta":"Dioxido de carbono","de":0,"hasta":20},{"etiqueta":"Hidrocarburos","de":0,"hasta":10000},{"etiqueta":"Oxigeno","de":0,"hasta":22}]
-
-// let limits = {"Parametro CO en rango estandar":10,"Parametro CO2 en rango estandar":20,"Parametro HC en rango estandar":10000,"Parametro O2 en rango estandar":22}
-
-// Esqueleto del controlador
-app.get("/api/emission-measurement/ranges_parameters", (req, res) => {
-    res.json(ranges_parameters);
-});
-
-app.get("/api/emission-measurement/limits", (req, res) => {
-  // Pruebas con el bot 
-  // res.send({
-  //   "limits": limits
-  // });
-});
-
-app.post("/api/emission-measurement/ranges_parameters", (req, res) => {
-  ranges_parameters.push(req.body);
-  res.json(ranges_parameters);
-
-});
-
-// const port = 3000;
-// app.listen(port, () => { console.log(`Example app listening at http://localhost:${port}`) })
 
 
-module.exports = app;
+const nombreGasMin = async () => {
+  let response =  await fetch("https://misiontic2022upb.vercel.app/api/emission-measurement/ranges-parameters")
+    .then(response => response.json())
+    .catch(error => console.log("Error"));
+
+  let nombreBajo = response[0].etiqueta;
+  let valorBajo = response[0].hasta;
+
+  for (const element of response) {
+    if (element.hasta <= valorBajo) {
+      nombreBajo = element.etiqueta;
+      valorBajo = element.hasta; 
+    }
+  }
+  return nombreBajo;
+}
+
+module.exports.nombreGasMin = nombreGasMin;
